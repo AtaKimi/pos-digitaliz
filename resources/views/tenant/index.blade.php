fd67573fe4411d8bf50053348ec6362c9db78225
roihan365 <bitcoiner.banjarmasin@gmail.com>
2023-09-20 09:29:28 +0800

    search tenant page

--- resources/views/tenant/index.blade.php
+++ resources/views/tenant/index.blade.php
@@ -148,7 +148,26 @@
 
       {{-- Table Order Start --}}
       <div class="relative mt-10 overflow-x-auto shadow-md sm:rounded-lg bg-white-50">
-        <h3 class="p-6 font-bold">Order list</h3>
+        <div class="flex flex-wrap gap-5 p-5 justify-between items-center">
+          <h1 class="text-xl text-gray-900 font-bold">
+              Order List
+          </h1>
+          <div class="pb-4 dark:bg-gray-900">
+              <label for="table-search" class="sr-only">Search</label>
+                  <div class="relative mt-1">
+                      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                          <svg class="w-4 h-4 text-gray-400 dark:text-gray-400" aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                          </svg>
+                      </div>
+                      <input type="text" id="table-search"
+                          class="block p-2 pl-10 text-sm text-gray-900 border-none focus:ring-0 rounded-lg w-80 bg-gray-100 dark:bg-gray-700 placeholder-gray-400 dark:text-white-50 "
+                          placeholder="Search">
+                  </div>
+              </div>
+          </div>
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
