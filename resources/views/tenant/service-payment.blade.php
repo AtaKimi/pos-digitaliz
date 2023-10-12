80736182e737b9ad143f7d28de606bd973a2f42f
Muhammad Bilal <muhammadbilalll2468@gmail.com>
2023-10-12 08:44:34 +0800

    bilal

--- resources/views/tenant/service-payment.blade.php
+++ resources/views/tenant/service-payment.blade.php
@@ -35,12 +35,11 @@
             <h4 class="text-lg font-semibold">Daftar Tagihan</h4>
             <div class="flex gap-4">
                 <form>
-                    <label for="default-search"
-                        class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3  pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
@@ -169,7 +168,7 @@
 
     {{-- MODAL TRANSACTION --}}
     <!-- Modal -->
-    <div data-te-modal-init data-te-backdrop="false"
+    {{-- <div data-te-modal-init data-te-backdrop="false"
         class="static left-0 top-0 z-[1055] block h-full w-full overflow-y-auto overflow-x-hidden outline-none"
         id="exampleModalComponents" tabindex="-1" aria-labelledby="exampleModalComponentsLabel" aria-hidden="true">
         <div data-te-modal-dialog-ref
@@ -209,5 +208,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
